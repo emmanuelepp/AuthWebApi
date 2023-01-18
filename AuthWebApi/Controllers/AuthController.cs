0f3b8f84bebf5582b44714e6acb32c5c4c64749b
Emma <emmanuelponciano@gmail.com>
2023-01-18 00:52:18 -0500

    Add project files.

---
+++ AuthWebApi/Controllers/AuthController.cs
@@ -0,0 +1,19 @@
+﻿using AuthWebApi.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AuthWebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        [HttpPost]
+        public async Task<ActionResult<User>> RegisterUSer(UserDto request) 
+        {
+            var user = new User {UserName = request.UserName};
+
+         return Ok(user);
+        }
+    }
+}
---
+++ AuthWebApi/Models/UserDto.cs
@@ -0,0 +1,8 @@
+﻿namespace AuthWebApi.Models
+{
+    public class UserDto
+    {
+        public string  UserName { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+    }
+}
